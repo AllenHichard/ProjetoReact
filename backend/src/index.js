78e58bdadaad5f843d89e36a014600e6e377b6ab
allenhichard <allenhichard21@gmail.com>
2020-01-18 16:25:27 -0300

    Finalizando o frontend da aplicação react

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const routes = require('./routes')
+const routes = require('./routes');
+const cors = require("cors") // para liberar a porta
 
 const app = express();
 const url = "mongodb://testeOmni:Maiden66@cluster0-shard-00-00-4d3je.mongodb.net:27017,cluster0-shard-00-01-4d3je.mongodb.net:27017,cluster0-shard-00-02-4d3je.mongodb.net:27017/week10?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority"
@@ -10,12 +11,17 @@ const paramet = {
 }
 mongoose.connect(url, paramet);
 
-
+app.use(cors());// adicionado para liberar todo acesos externo sem restrição
 
 app.use(express.json()); // para entender Json
 app.use(routes);
 
 app.listen(3333);
+
+
+
+
+
 
 
 
